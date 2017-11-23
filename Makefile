6eee137826f8263dbb23ad68e8dd7122f1385061
JKS <rltjr1948@kpu.ac.kr>
2017-11-24 01:24:22 +0900

    PID, LWP, Makefile

---
+++ Makefile
@@ -0,0 +1,18 @@
+# Makefile
+
+# 파일명을 입력 (확장자 빼고)
+FILE1=main
+
+OBJS=$(FILE1)
+
+CC=gcc
+CFLAGS=
+LIBS=
+
+all: $(FILE1)
+
+$(FILE1):	$(FILE1).c
+	$(CC) $(FILE1).c -o $(FILE1)
+
+c:
+	rm -f $(OBJS) output core
--- main.c
+++ main.c
@@ -1,6 +1,58 @@
-#include <stdio.h>
+#include <dirent.h> 
+#include <errno.h> 
+#include <fcntl.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <procfs.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-int main(void)
+void openPsinfo(int pid);
+void openCmdline(int pid);
+
+int main(void) {
+	DIR *dp;
+	struct dirent *dent;
+    char dirName[] = "/proc";
+
+	dp = opendir(dirName);
+    if(dp == NULL) {
+		printf("ERROR: Could not open directory(%s).\n", dirName);
+		exit(10);
+	}
+
+	while((dent=readdir(dp)))
+	{
+		printf("프로세스 PID : %d\n", atoi(dent->d_name));
+		openPsinfo(atoi(dent->d_name));
+	}
+
+    if(closedir(dp) < 0) {
+		printf("ERROR: Could not close directory(%s).\n", dirName);
+		exit(10);
+	}
+
+	exit(0);
+}
+
+void openPsinfo(int pid)
 {
-	return 0;
+	int fd;
+	char fileName[1024]; //proc/PID/각종 정보파일
+	char buffer[512];
+	psinfo_t data;
+	int lwp=0;
+
+	memset(&data, 0, sizeof(psinfo_t));
+	sprintf(fileName, "/proc/%d/psinfo", pid);
+	if((fd = open(fileName, O_RDONLY)) < 0) 
+	{
+		printf("ERROR: %s open failed\n", fileName);
+	}
+	read(fd, &data, sizeof(psinfo_t));
+
+	lwp = data.pr_nlwp;
+	printf("프로세스의 활성 LWP 수 : %d\n", lwp);
+	close(fd);
 }
