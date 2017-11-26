381a6814433c05bf46aff80562567c568690beb8
HyeonMyeong Jang <gkagm2@gmail.com>
2017-11-26 23:09:16 +0900

    option.c 수정

--- option.c
+++ option.c
@@ -1,27 +1,21 @@
 #include "stdhdr.h"
 
 //현재 실행되고있는 파일 구조체를 가져온다.
-int  optKill(topData *top ){
+short  optKill( ){
 	int targetPID;
+	short flag;
+
+
 
 	printf("PID to signal/kill [default pid = 22599] ");
 	scanf("%d",&targetPID);
-	printf("dd%d",targetPID);
+	if(flag = kill(targetPID, SIGKILL) == -1){
+		printf("%d PID is not exist \n",targetPID);
+	} else { 
+		printf("%d PID is deleted \n",targetPID);
+	}
 
 	
-//	if(){
-
-//	}else {
-		
-//	}
 	return targetPID;
 }
 
-//프로그램 종료
-int optEnd(char d){
-	printf("%c",d);
-	if( d == 'q') { //q를 누르면 1을 리턴 
-		return 1;
-	}
-	return 0; //아니면 0을 리턴
-}
