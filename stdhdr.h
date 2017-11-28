8d36e2415f367d72623b0e0df4b838f0888fcd53
HyeonMyeong Jang <gkagm2@gmail.com>
2017-11-28 12:09:08 +0900

    ReadMe.txt 수정 꼭 읽어보세요 stdhdr.h 주석 추가

--- stdhdr.h
+++ stdhdr.h
@@ -17,50 +17,48 @@
 
 //define
 //OptSort()함수의 signal
-#define SORT_SIZE 1
-#define SORT_PID 2
-#define SORT_RES 3
+#define SORT_SIZE 1 //size별로 정렬할 때
+#define SORT_PID 2 //pid별로 정렬할 때
+#define SORT_RES 3 //res별로 정렬할 때
 
 //PrintPsInfo()함수의 signal
-#define FRONT_PAGE 1
-#define BACK_PAGE 2
-#define INIT_PAGE 3 //업데이트 할 시 초기화 시그널
-#define CURRENT_PAGE 4
+#define FRONT_PAGE 1 //이후 페이지로 넘어갈 때 
+#define BACK_PAGE 2 //이전 페이지로 넘길 때
+#define INIT_PAGE 3 //업데이트 할 시 초기화
+#define CURRENT_PAGE 4 //현재 페이지를 출력하고 싶을 때
 
 //받아올 프로세스 한정치
 #define MAX_DATA_SIZE 400
 
 //화면에 보여줄 top 구조체 요소.
 typedef struct top{
-	int pid;
-	int lwp;
-	char command[64];
-	char time[8];
-	int size;
+	int pid; //프로세스 id
+	int lwp; //
+	char command[64]; //명렁어
+	char time[8]; //시간
+	int size; //프로세스 size
 	int res;
 	int mem;
 } topData;
 
-//data에 가져온 프로세스 개수.
-static int processCount=0;
+static int processCount=0; //data에 가져온 프로세스 개수
 
 //funciton
-void PrintData(int pid, topData *data);
-void SearchData(char *str, topData *data);
-struct top OpenPsinfo(int);
-void PrintPsInfo(topData *data, int signal);
-void PrintMainInfo();
-void GetPsInfo(topData *data);
-void ChangePos(topData *a, topData *b); 
-void ClearReadBuffer();
-void InitData(topData *data);
-void GetDataSize(topData *data);
-int Getch(void);  
-struct top OpenPsinfo(int pid);
-void PrintHelpInfo();
+void PrintData(int pid, topData *data); //process 값을 출력해줌.
+void SearchData(char *str, topData *data); //특정 프로세스 정보를 찾기위한 함수.
+void PrintPsInfo(topData *data, int signal); //프로세스 정보를 출력한다.
+void PrintMainInfo(); //top 프로그램의 기본 정보 및 주요내용 출력
+void GetPsInfo(topData *data); //data에 프로세스 정보 받아오기.
+struct top OpenPsinfo(int pid); //process의 정보를 열기.
+void ChangePos(topData *a, topData *b); //구조체 바꾸기
+void ClearReadBuffer(); //버퍼를 비우기 위한 함수.
+void InitData(topData *data); //Data의 배열을 초기화 하기위한 함수
+void GetDataSize(topData *data); //지울수도 있음
+int Getch(void); //지울수도 있음 
+void PrintHelpInfo(); //Top 사용법과 옵션 설명
 
 //option fucntion
-void OptKill();
+void OptKill(); //특정 프로세를 종료시키기 위한 함수
 int OptSort(topData *data,int size, int flag);
 
 
